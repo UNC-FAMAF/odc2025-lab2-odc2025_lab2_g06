f89dafbf10676a08eaf82e235cad4958bf5f5eb8
freeguada <guadaluperocioacosta@mi.unc.edu.ar>
2025-05-23 23:51:29 +0000

    [FIX] frame 5

--- ejercicio2/app.s
+++ ejercicio2/app.s
@@ -6,18 +6,7 @@
      
 main:
 
-	 bl draw_background_5
-	 bl draw_blue_rainbow_5
-	 bl draw_green_rainbow_5
-	 bl draw_red_rainbow_5
-     bl draw_orange_rainbow_5
-     bl draw_purple_rainbow_5
-	 bl draw_yellow_rainbow_5
-     bl draw_letras_5
-     bl draw_stars_5
-     bl draw_toes_5
-     bl draw_body_5
-	 bl draw_tail_5
+	 bl frame_5
 
 	.globl main
 
--- ejercicio2/draw_frames.s
+++ ejercicio2/draw_frames.s
@@ -9,6 +9,19 @@
 
 
 frame_5:
+     bl draw_background_5
+	 bl draw_blue_rainbow_5
+	 bl draw_green_rainbow_5
+	 bl draw_red_rainbow_5
+     bl draw_orange_rainbow_5
+     bl draw_purple_rainbow_5
+	 bl draw_yellow_rainbow_5
+     bl draw_letras_5
+     bl draw_stars_5
+     bl draw_toes_5
+     bl draw_body_5
+	 bl draw_tail_5
+ret
 frame_3:
 
 
