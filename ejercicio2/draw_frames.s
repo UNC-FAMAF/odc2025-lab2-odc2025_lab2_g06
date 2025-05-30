f48459046e9a50b55dafa5b0162b5977cab0ff92
justkope <carladiaz26@mi.unc.edu.ar>
2025-05-30 12:26:37 +0000

    [ADD] Docs
    Added new doc, and fixed a funtion that appears in two archives

--- ejercicio2/draw_frames.s
+++ ejercicio2/draw_frames.s
@@ -154,30 +154,4 @@ sub sp, sp, #48
     add sp, sp, #48
 ret
 
-draw_clean:
-    sub sp, sp, #48
-    str x1, [sp]
-    str x2, [sp, #8]
-    str x3, [sp, #16]
-    str x4, [sp, #24]
-    str x7, [sp, #32]
-    str x30, [sp, #40]
-
-    mov x1, 0       
-    mov x2, 0    
-    mov x3, SCREEN_WIDTH        
-    mov x4, SCREEN_HEIGH
-    ldr x7, =DARK_BLUE
-    bl draw_rectangle
-
-    ldr x30, [sp, #40]
-    ldr x7, [sp, #32]
-    ldr x4, [sp, #24]
-    ldr x3, [sp, #16]
-    ldr x2, [sp, #8]
-    ldr x1, [sp]
-    add sp, sp, #48
-
-ret
-
 
